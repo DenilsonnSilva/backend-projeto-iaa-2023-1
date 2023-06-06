53a0cf494381a73862a2cd0c89ea9d08f7038cd8
Denilson Arruda da Silva <denilsonarruda@outlook.com>
2023-06-06 04:11:28 -0300

    Added prediction route

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from keras.preprocessing import image
 app = Flask(__name__)
 model = load_model("path/to/your/model.h5")
 
-if __name__ == "__main__":
-    app.run()
 
 def preprocess(img_array):
     img_resized = np.array(Image.fromarray(img_array).resize((224, 224)))
     img_normalized = img_resized / 255.0
     img_expanded = np.expand_dims(img_normalized, axis=0)
     return img_expanded
+
 
 def get_class_info(prediction):
     classes = {
@@ -23,3 +22,22 @@ def get_class_info(prediction):
         2: ("class_id_2", "Caminhão"),
     }
     return classes[prediction]
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "image" not in request.files:
+        return jsonify({"error": "Não foi encontrada nenhuma imagem"})
+
+    file = request.files["image"]
+    img = Image.open(file.stream)
+    img_array = np.array(img)
+    img_processed = preprocess(img_array)
+    prediction = model.predict(img_processed)
+    class_id, class_name = get_class_info(np.argmax(prediction))
+    response = {"class_id": class_id, "class_name": class_name}
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run()
